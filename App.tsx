2a775f097420c509d625a77958073e47c11b338a
Viet Anh Tran <tranvietanh1996@gmail.com>
2022-07-03 14:58:00 +1200

    Add home page

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Home } from "./src/Home";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Home />
     </SafeAreaProvider>
   );
 }
